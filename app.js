270d21a28c1eb1cdf930760cc722178231be2475
shrestha <shresthachakrabortykgp@gmail.com>
2025-02-13 09:57:29 +0530

    socketio bolierplate configured

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const socketio = require("socketio");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
 app.get("/", function (req, res) {
   res.send("hey");
 });
-const server = http.createServer(app);
-app.listen(3000);
+
+server.listen(3000);
